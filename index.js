0688cbd44e10393f056e747042484ab9cf0098a0
havizh <havizhabdurrahman@gmail.com>
2023-09-08 12:19:22 +0700

    + js

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = prompt("what's your name?")
+let age = prompt("How old are you?")
+let food = prompt("What's your favorite food?")
+
+// alert(`name : ${name}
+// age : ${age}
+// favorite food : ${food}`)
+
+alert(`HELLO "${name}"
+WELCOME TO MY FIRST WEBSITE`)
+alert('PLEASE ENJOY')
